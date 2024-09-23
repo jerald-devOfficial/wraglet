65df4c0415c15d8839ebb312c805f66c41ddc35d
jerald-devOfficial <spaueOfficial@gmail.com>
2024-09-23 21:11:25 +0800

    feat: switch to yarn berry from npm, setup turbopack config, add module resolutions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ['ably', 'mongoose'],
+      turbo: {
+        rules: {
+          '*.svg': {
+            loaders: ['@svgr/webpack'],
+            as: '*.js',
+          },
+        },
+        resolveAlias: {
+          underscore: 'lodash',
+          mocha: { browser: 'mocha/browser-entry.js' },
+        },
+        resolveExtensions: [
+          '.mdx',
+          '.tsx',
+          '.ts',
+          '.jsx',
+          '.js',
+          '.mjs',
+          '.json',
+        ],
+        moduleIdStrategy: 'deterministic',
+      }
+    }
+  };
+  
+  module.exports = nextConfig;
+  
