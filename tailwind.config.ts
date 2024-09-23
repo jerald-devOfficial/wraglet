ece07406a9dcf032dc97ff5855705a1a0c7ec6af
jerald-devOfficial <spaueOfficial@gmail.com>
2024-09-23 23:13:13 +0800

    feat: update tailwind config to extend screen and add plugin tailwind forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      screens: {
+        tablet: '900px',
+        // => @media (min-width: 900px) { ... }
+        '3xl': '1920px'
+        // => @media (min-width: 1920px) { ... }
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-};
-export default config;
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/forms')({
+      strategy: 'class'
+    })
+  ]
+}
+export default config
