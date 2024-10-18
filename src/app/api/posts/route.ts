43dc9a35e7b4bb6909efc34901378f44e76c21ca
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-19 01:21:27 +0800

    feat: create routes for ably token, posting routes for with image and postId for reaction

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,89 @@
+import { NextResponse } from 'next/server'
+import getCurrentUser from '@/actions/getCurrentUser'
+import dbConnect from '@/lib/dbConnect'
+import Post from '@/models/Post'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { v4 as uuidv4 } from 'uuid'
+
+export const POST = async (request: Request) => {
+  const s3Client = new S3Client({
+    endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    region: 'auto',
+    credentials: {
+      accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID ?? '',
+      secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY ?? ''
+    }
+  })
+
+  try {
+    await dbConnect()
+
+    const currentUser = await getCurrentUser()
+    const body = await request.json()
+    const { text, audience = 'public', image } = body
+
+    if (!currentUser?._id || !currentUser?.email) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const uploadImageToR2 = async (
+      image: string
+    ): Promise<{ url: string; key: string }> => {
+      const base64Data = Buffer.from(
+        image.replace(/^data:image\/\w+;base64,/, ''),
+        'base64'
+      )
+      const type = image.split(';')[0].split('/')[1]
+      const key = `user/post/${uuidv4()}.${type}`
+      const bucketName = process.env.CLOUDFLARE_R2_BUCKET_NAME
+
+      const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        Body: base64Data,
+        ContentType: `image/${type}`
+      })
+
+      await s3Client.send(command)
+      const url = `${process.env.NEXT_PUBLIC_R2_USERS_URL}/${key}`
+      return { url, key }
+    }
+
+    let content = {}
+
+    if (text && image) {
+      const imageUrl = await uploadImageToR2(image)
+      content = { text, images: [imageUrl] }
+    } else if (text) {
+      content = { text }
+    } else if (image) {
+      const imageUrl = await uploadImageToR2(image)
+      content = { images: [imageUrl] }
+    } else {
+      return new NextResponse('Text or image is required', { status: 400 })
+    }
+
+    const newPost = await Post.create({
+      content,
+      audience,
+      author: currentUser._id
+    })
+
+    const populatedPost = await Post.findById(newPost._id)
+      .populate({
+        path: 'author',
+        select:
+          'id firstName lastName email dob gender bio pronoun profilePicture coverPhoto createdAt updatedAt'
+      })
+      .exec()
+
+    return NextResponse.json(populatedPost)
+  } catch (err: any) {
+    console.log('Fetching posts error: ', err)
+    console.error(
+      'Error happened while doing POST for /api/posts at route.ts: ',
+      err
+    )
+    return new NextResponse('Internal Error: ', { status: 500 })
+  }
+}
---
+++ src/app/api/token/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+// import getCurrentUser from '@/actions/getCurrentUser'
+import Ably from 'ably'
+
+export const GET = async () => {
+  try {
+    // const currentUser = await getCurrentUser()
+    if (!process.env.ABLY_API_KEY) {
+      return NextResponse.json(
+        {
+          errorMessage: `Missing ABLY_API_KEY environment variable.
+        If you're running locally, please ensure you have a ./.env file with a value for ABLY_API_KEY=your-key.
+        If you're running in Netlify, make sure you've configured env variable ABLY_API_KEY. 
+        Please see README.md for more details on configuring your Ably API Key.`
+        },
+        {
+          status: 500,
+          headers: new Headers({
+            'content-type': 'application/json'
+          })
+        }
+      )
+    }
+
+    const client = new Ably.Rest(process.env.ABLY_API_KEY)
+    // Removed 'await' as createTokenRequest is not a promise
+    const tokenRequestData = await client.auth.createTokenRequest({
+      clientId: 'wraglet-ably'
+      // clientId: currentUser?._id
+    })
+    return NextResponse.json(tokenRequestData)
+  } catch (error: any) {
+    console.log('Accessing Ably token route error: ', error)
+    console.error(
+      'Error happened while doing POST for /api/token at route.ts: ',
+      error
+    )
+    return new NextResponse('Internal Error: ', { status: 500 })
+  }
+}
