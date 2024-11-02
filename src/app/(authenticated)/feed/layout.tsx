1613cdb43f35d8cbe1eeb943e82f695ff730d1f8
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-03 03:15:46 +0800

    feat: create an AuthenticatedLayout, wrap both feed and [username] pages as they have common layout with Header, delete layout in [username]

--- src/app/(authenticated)/feed/layout.tsx
+++ src/app/(authenticated)/feed/layout.tsx
@@ -1,10 +1,4 @@
 import type { Metadata } from 'next'
-import getCurrentUser from '@/actions/getCurrentUser'
-import deJSONify from '@/utils/deJSONify'
-
-import Header from '@/components/Header'
-
-export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Wraglet Feed - Explore Impactful Connections',
@@ -35,21 +29,7 @@ export const metadata: Metadata = {
 }
 
 const FeedLayout = async ({ children }: { children: React.ReactNode }) => {
-  const jsonUser = await getCurrentUser().catch((err) => {
-    console.error(
-      'Error happened while getting getCurrentUser() on Feed component: ',
-      err
-    )
-  })
-
-  const currentUser = deJSONify(jsonUser)
-
-  return (
-    <div className="relative flex min-h-screen flex-col items-center overflow-hidden bg-[rgba(110,201,247,0.15)]">
-      <Header currentUser={currentUser} />
-      {children}
-    </div>
-  )
+  return children
 }
 
 export default FeedLayout
